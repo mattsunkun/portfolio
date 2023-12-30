01356505f407fbb91bed5575f27fdddd13a5a378
mattsunkun <mattsunkun1221@gmail.com>
2023-12-30 11:24:14 +0900

    <basing> before cards

--- src/data/skill.ts
+++ src/data/skill.ts
@@ -19,7 +19,7 @@ const language: tSkill[] = [
   {
     id: "idPython",
     title: "Python",
-    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+    img: `${process.env.PUBLIC_URL}/images/skills/icon/python/python-original.svg`,
     start: new Date(2021, 4, 1),
     description: "大学の授業で初めて触った言語です．今でも機械学習や，競技プログラミングにおいてよく使う言語です．",
     priority: 2,
@@ -31,9 +31,9 @@ const language: tSkill[] = [
   {
     id: "idJs",
     title: "JavaScript",
-    img: "",
+    img: `${process.env.PUBLIC_URL}/images/skills/icon/javascript/javascript-original.svg`,
     start: new Date(2021, 4, 1),
-    description: "初めての開発バイトや，拡張機能の作成に利用しました．",
+    description: "独特な性質にあまり慣れない言語です．初めての開発バイトや，拡張機能の作成に利用しました．",
     priority: 2,
 
     exp: -1,
@@ -44,76 +44,76 @@ const language: tSkill[] = [
 
 const framework: tSkill[] = [
   {
-    id: "idPython",
-    title: "Python",
-    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+    id: "idReact",
+    title: "React",
+    img: `${process.env.PUBLIC_URL}/images/skills/icon/react/react-original.svg`,
     start: new Date(2021, 4, 1),
-    description: "大学の授業で初めて触った言語です．今でも機械学習や，競技プログラミングにおいてよく使う言語です．",
+    description: "私はReact単体で使うというよりは，MAUIを使って，TypeScriptで記述しています．",
     priority: 2,
 
     exp: -1,
-    officialUrl: "https://www.python.org/",
-    skill: eSkill.language,
+    officialUrl: "https://react.dev/",
+    skill: eSkill.framework,
   },
 ];
 
 const library: tSkill[] = [
   {
-    id: "idPython",
-    title: "Python",
-    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+    id: "idNumpy",
+    title: "Numpy",
+    img: `${process.env.PUBLIC_URL}/images/skills/icon/numpy/numpy-original.svg`,
     start: new Date(2021, 4, 1),
-    description: "大学の授業で初めて触った言語です．今でも機械学習や，競技プログラミングにおいてよく使う言語です．",
+    description: "Pythonで数値を扱うときはよく使ってます．特に行列が絡むと無意識に使いがちです．",
     priority: 2,
 
     exp: -1,
-    officialUrl: "https://www.python.org/",
-    skill: eSkill.language,
+    officialUrl: "https://numpy.org/",
+    skill: eSkill.library,
   },
 ];
 
 const software: tSkill[] = [
   {
-    id: "idPython",
-    title: "Python",
-    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+    id: "idGit",
+    title: "Git",
+    img: `${process.env.PUBLIC_URL}/images/skills/icon/git/git-original.svg`,
     start: new Date(2021, 4, 1),
-    description: "大学の授業で初めて触った言語です．今でも機械学習や，競技プログラミングにおいてよく使う言語です．",
+    description: "個人開発において，(ほぼ)必ず使うバージョン管理ツールです．",
     priority: 2,
 
     exp: -1,
-    officialUrl: "https://www.python.org/",
-    skill: eSkill.language,
+    officialUrl: "https://git-scm.com/",
+    skill: eSkill.software,
   },
 ];
 
 const hardware: tSkill[] = [
   {
-    id: "idPython",
-    title: "Python",
-    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+    id: "idRaspberryPi",
+    title: "Raspberry PI",
+    img: `${process.env.PUBLIC_URL}/images/skills/icon/raspberrypi/raspberrypi-original.svg`,
     start: new Date(2021, 4, 1),
-    description: "大学の授業で初めて触った言語です．今でも機械学習や，競技プログラミングにおいてよく使う言語です．",
+    description: "LAN，OSなどの幅広いお勉強に役立てています．",
     priority: 2,
 
     exp: -1,
-    officialUrl: "https://www.python.org/",
-    skill: eSkill.language,
+    officialUrl: "https://www.raspberrypi.com/",
+    skill: eSkill.hardware,
   },
 ];
 
 const platform: tSkill[] = [
   {
-    id: "idPython",
-    title: "Python",
-    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+    id: "idAws",
+    title: "Amazon Web Services",
+    img: `${process.env.PUBLIC_URL}/images/skills/icon/amazonwebservices/amazonwebservices-original.svg`,
     start: new Date(2021, 4, 1),
-    description: "大学の授業で初めて触った言語です．今でも機械学習や，競技プログラミングにおいてよく使う言語です．",
+    description: "Lambda，ApiGatewayとかが好きです．",
     priority: 2,
 
     exp: -1,
-    officialUrl: "https://www.python.org/",
-    skill: eSkill.language,
+    officialUrl: "https://aws.amazon.com/",
+    skill: eSkill.platform,
   },
 ];
 
