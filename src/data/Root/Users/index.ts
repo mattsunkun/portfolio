865761f3c963b37e75aa381fad837df201771fc1
mattsunkun <mattsunkun1221@gmail.com>
2024-01-24 22:51:30 +0900

    <add><bugfix> .naと.isoのリンクを追加．scikit-learnのparsing errorを直しました．

--- src/data/Root/Users/index.ts
+++ src/data/Root/Users/index.ts
@@ -6,12 +6,26 @@ const dirUsers: directory = {
   name: "Users",
   files: [
     {
+      name: ".iso",
+      contents: 'A friend since High School.\nYou can jump to his Portfolio with a "open" command!',
+      meta: {
+        urls: ["https://iso1216.github.io/portfolio",],
+      },
+    },
+    {
       name: ".maron",
       contents: 'A friend since University.\nYou can jump to his Portfolio with a "open" command!',
       meta: {
         urls: ["https://kisako-riku-portfolio.vercel.app",],
       },
-    }
+    },
+    {
+      name: ".na",
+      contents: 'A friend since University.\nYou can jump to her Portfolio with a "open" command!',
+      meta: {
+        urls: ["https://nac-39.com/",],
+      },
+    },
   ],
   directories: [
     dirAdmin,
