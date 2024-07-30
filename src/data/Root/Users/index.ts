860777b601e3834f08a2f5d33e6044d1e6c7185b
mattsunkun <mattsunkun1221@gmail.com>
2024-07-30 16:16:12 +0900

    <mod> a -> an

--- src/data/Root/Users/index.ts
+++ src/data/Root/Users/index.ts
@@ -7,35 +7,35 @@ const dirUsers: directory = {
   files: [
     {
       name: ".iso",
-      contents: 'A friend since High School.\nYou can jump to his Portfolio with a "open" command!',
+      contents: 'A friend since High School.\nYou can jump to his Portfolio with an "open" command!',
       meta: {
         urls: ["https://iso1216.github.io/portfolio",],
       },
     },
     {
       name: ".kawachan",
-      contents: 'A friend since University.\nYou can jump to his Portfolio with a "open" command!',
+      contents: 'A friend since University.\nYou can jump to his Portfolio with an "open" command!',
       meta: {
         urls: ["https://kawachann.com/",],
       },
     },
     {
       name: ".maron",
-      contents: 'A friend since University.\nYou can jump to his Portfolio with a "open" command!',
+      contents: 'A friend since University.\nYou can jump to his Portfolio with an "open" command!',
       meta: {
         urls: ["https://kisako-riku-portfolio.vercel.app",],
       },
     },
     {
       name: ".nahco3",
-      contents: 'A friend since University.\nYou can jump to her Portfolio with a "open" command!',
+      contents: 'A friend since University.\nYou can jump to her Portfolio with an "open" command!',
       meta: {
         urls: ["https://nac-39.com/",],
       },
     },
     {
       name: ".powell",
-      contents: 'A friend since University.\nYou can jump to his Portfolio with a "open" command!',
+      contents: 'A friend since University.\nYou can jump to his Portfolio with an "open" command!',
       meta: {
         urls: ["http://portfolio.pwll.dev/",],
       },
